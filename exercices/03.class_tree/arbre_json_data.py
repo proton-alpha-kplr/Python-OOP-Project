2fb286e654a4ce8c200c53213deb2383e02a7161
proton-alpha-kplr <124889858+proton-alpha-kplr@users.noreply.github.com>
2023-03-07 09:53:00 +0000

    PaulineS

--- exercices/03.class_tree/arbre_json_data.py
+++ exercices/03.class_tree/arbre_json_data.py
@@ -39,7 +39,7 @@ def create_tree_from_dict(json_dict):
     # Créer un nouvel arbre
     mytree = Tree()
     # Créer le noeud racine de l'arbre
-    mytree.create_node(tag="Racine", identifier="racine")
+    mytree.create_node(tag="Product Classes Hierarchy", identifier="racine")
     
     # Parcourir récursivement le dictionnaire Python pour créer les noeuds de l'arbre (fonction ci dessous)
     recursive_tree_from_json(mytree, json_dict, "racine")
@@ -50,6 +50,7 @@ def create_tree_from_dict(json_dict):
 # Fonction récursive pour parcourir un dictionnaire Python et créer des noeuds dans un arbre
 def recursive_tree_from_json(tree, json_dict, parent_node_id):
     for key, value in json_dict.items():
+        #Si la clé est une subclasse, on refait la fonction récursive mais avec "value" de la subclass comme dictionnaire principal
         if (key == "subclasses"):
             recursive_tree_from_json(tree, value, parent_node_id)
         elif (isinstance(value, dict) ):  #enlever les valeur nulles
@@ -59,8 +60,7 @@ def recursive_tree_from_json(tree, json_dict, parent_node_id):
             
             # Créer récursivement le sous-arbre pour le dictionnaire courant
             recursive_tree_from_json(tree, value, new_node_id)
-        #elif not (isinstance(value, dict)):
-        #    pass
+        #on ne met pas d'autre else, car on ne veut rien faire on arrive à une value d'un dictionnaire qui n'est ni une subclasse, ni un dictionnaire (ex les attributs)
 
     # Pour chaque clé (class_name) et valeur (class_attrs) dans le dictionnaire :
     #     Créer un nouveau noeud pour la clé courante du dictionnaire (nom de la classe)
